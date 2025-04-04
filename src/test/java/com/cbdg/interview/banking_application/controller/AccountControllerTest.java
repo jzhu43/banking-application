c3f06bb69bf9926211811d4cc91bf5ad1788f471
jzhu43 <jzhu43@ucmerced.edu>
2025-04-04 14:52:42 -0700

    Test files for controllers

---
+++ src/test/java/com/cbdg/interview/banking_application/controller/AccountControllerTest.java
@@ -0,0 +1,11 @@
+package com.cbdg.interview.banking_application.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AccountControllerTest {
+
+     public AccountControllerTest() {
+    }
+
+}
---
+++ src/test/java/com/cbdg/interview/banking_application/controller/TransactionControllerTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+package com.cbdg.interview.banking_application.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+ 
+/**
+ *
+ * @author jason
+ */
+public class TransactionControllerTest {
+
+     public TransactionControllerTest() {
+    }
+
+}
